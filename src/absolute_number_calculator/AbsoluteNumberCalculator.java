2921df72d6b3618d22d64d38bc0b524850108a3f
nguyenthanhlongvn03 <nguyenthanhlongvn03@gmail.com>
2026-01-20 10:43:56 +0700

    add

---
+++ src/absolute_number_calculator/AbsoluteNumberCalculator.java
@@ -0,0 +1,8 @@
+package absolute_number_calculator;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number) {
+        return -number;
+        //throw new UnsupportedOperationException();
+    }
+}
